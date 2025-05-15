d152ea85893e28c047468bc1dcef1bf0411b67ab
mydouleur <mydouleur@icloud.com>
2025-05-15 22:14:00 +0800

    提交控件基本的虚类定义

---
+++ Controls/RootControl.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NullMenu.Controls
+{
+    public abstract class RootControl
+    {
+        public RootControl Parent { get; set; }
+        public List<RootControl>Children { get; set; }
+        public abstract void Render();
+        public EventHandler MouseMove { get; set; }
+        public EventHandler MouseDown { get; set; }
+        public EventHandler MouseUp { get; set; }
+
+        public abstract bool IsTriggered { get; set; }
+    }
+}
