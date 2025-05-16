6b3b0228bf447f89fc02a04471d112b182873f3e
mydouleur <mydouleur@icloud.com>
2025-05-16 12:19:05 +0800

    调整了tags控件，现在能正式作为一个有背景的label来使用了

--- Controls/NullTag.cs
+++ Controls/NullTag.cs
@@ -1,26 +1,30 @@
-﻿using Raylib_cs;
-
+﻿using NullMenu.Enum;
+using Raylib_cs;
+using System.Numerics;
 namespace NullMenu.Controls
 {
     public class NullTag : RootControl
     {
-        public int PosX { get; set; } = 50;
-        public int PosY { get; set; } = 100;
-        public int Width { get; set; } = 100;
-        public int Height { get; set; } = 50;
-        public string Text { get; set; } = "Text";
-        public int fontSize { get; set; } = 20;
+        public float PosX { get; set; } = 50;
+        public float PosY { get; set; } = 100;
+        public float Width { get; set; } = 100;
+        public float Height { get; set; } = 50;
+        public string Text { get; set; } = "BDDDDDS";
+        public int FontSize { get; set; } = 20;
+        public float Spacing { get; set; } = 2;
         public Color Background { get; set; } = Color.Blue;
         public Color Foreground { get; set; } = Color.White;
-
+        public Font Font { get; set; } = Raylib.GetFontDefault();
+        public HorizontalAlign HorizontalAlign { get; set; } = HorizontalAlign.Center;
         public override bool IsTriggered()
         {
             return true;
         }
         public override void Render()
         {
-            Raylib.DrawRectangle(PosX, PosY, Width, Height, Background);
-            Raylib.DrawText(Text, PosX, PosY, fontSize, Foreground);
+            Raylib.DrawRectangleV(new Vector2(PosX, PosY), new Vector2(Width, Height), Background);
+            var texLen = Raylib.MeasureText(Text, FontSize);
+            Raylib.DrawTextEx(Font, Text, new Vector2(PosX + (int)(HorizontalAlign) * 0.5f * (Width - texLen), PosY + 0.5f * Height - FontSize * 0.5f), FontSize, Spacing, Foreground);
             base.Render();
         }
     }
---
+++ Enum/HorizontalAlign.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NullMenu.Enum
+{
+    public enum HorizontalAlign
+    {
+        Left =0,
+        Center = 1,
+        Right = 2,
+    }
+}
